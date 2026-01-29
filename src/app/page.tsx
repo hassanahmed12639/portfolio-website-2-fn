5a62bc68188134f06297653d8557f55957001ba3
Hassan Ahmed <fluxseopro.com>
2026-01-29 15:48:41 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Header from '@/components/layout/Header'
 import Hero from '@/components/sections/Hero'
 import UnifyFinancesScroll from '@/components/sections/UnifyFinancesScroll'
-import WhoWeAreBento from '@/components/sections/WhoWeAreBento'
-import PartnerHero from '@/components/sections/PartnerHero'
 import IntegrationsArcSection from '@/components/sections/IntegrationsArcSection'
-import ServicesGradient from '@/components/sections/ServicesGradient'
 import PerformanceMarketingArchitecture from '@/components/sections/PerformanceMarketingArchitecture'
 
 export default function Home() {
@@ -15,14 +12,9 @@ export default function Home() {
       <div className="py-16 md:py-24">
         <UnifyFinancesScroll />
       </div>
-      <div className="py-10 md:py-16">
-        <WhoWeAreBento />
-      </div>
-      <PartnerHero />
       <div className="py-16 md:py-24">
         <IntegrationsArcSection />
       </div>
-      <ServicesGradient />
       <div className="py-16 md:py-24">
         <PerformanceMarketingArchitecture />
       </div>
