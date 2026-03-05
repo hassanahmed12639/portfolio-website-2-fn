18ecb4a1fe5402dfc70293023f0df32cbe85df3a
hassanahmed12639 <hassanonclouds@gmail.com>
2026-03-05 08:08:47 +0500

    f

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+      ],
+    },
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "img.clerk.com", pathname: "/**" },
