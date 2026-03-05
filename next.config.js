c5830cd5f93e9ffc6398c254360139c2692e6416
hassanahmed12639 <hassanonclouds@gmail.com>
2026-03-05 08:24:18 +0500

    Fix admin route group manifest error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   experimental: {
+    serverComponentsExternalPackages: [],
     outputFileTracingExcludes: {
       '*': [
         'node_modules/@swc/core-linux-x64-gnu',
