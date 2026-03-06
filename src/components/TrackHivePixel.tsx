6a7773081c479e14d5a28898e2847b35507319ad
hassanahmed12639 <hassanonclouds@gmail.com>
2026-03-06 07:14:35 +0500

    Add TikTok browser pixel + server side for full coverage

--- src/components/TrackHivePixel.tsx
+++ src/components/TrackHivePixel.tsx
@@ -19,17 +19,22 @@ export default function TrackHivePixel() {
     }
   }, [])
 
-  // PageView tracking on route changes
+  // PageView tracking on route changes (browser pixels + TrackHive server-side)
   useEffect(() => {
     if (!isTrackDomain) return
     if (typeof window === 'undefined') return
 
+    // Meta PageView
     if ((window as unknown as { fbq?: (a: string, b: string) => void }).fbq) {
       (window as unknown as { fbq: (a: string, b: string) => void }).fbq('track', 'PageView')
     }
+
+    // TikTok PageView
     if ((window as unknown as { ttq?: { page: () => void } }).ttq) {
       (window as unknown as { ttq: { page: () => void } }).ttq.page()
     }
+
+    // TrackHive server-side PageView
     if ((window as unknown as { trackhive?: (a: string, b: string, c?: { event_source_url: string }) => void }).trackhive) {
       (window as unknown as { trackhive: (a: string, b: string, c?: { event_source_url: string }) => void }).trackhive('track', 'PageView', {
         event_source_url: window.location.href,
@@ -63,12 +68,23 @@ export default function TrackHivePixel() {
       {/* TikTok Pixel Base Code */}
       <Script id="tiktok-pixel" strategy="afterInteractive">
         {`
-          !function (w, d, t) {
-            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-            ttq.load('${process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID || 'D6K5RJBC77U9T6VFJPK0'}');
-            ttq.page();
-          }(window, document, 'ttq');
-        `}
+    !function (w, d, t) {
+      w.TiktokAnalyticsObject=t;
+      var ttq=w[t]=w[t]||[];
+      ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+      ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+      for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+      ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
+      ttq.load=function(e,n){
+        var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
+        ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};
+        n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;
+        e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)
+      };
+      ttq.load('${process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID || 'D6K5RJBC77U9T6VFJPK0'}');
+      ttq.page();
+    }(window, document, 'ttq');
+  `}
       </Script>
 
       {/* TrackHive Server-Side Script */}
