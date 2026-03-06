afac2ac6c4b75ae727c5f95884586ecf24cc6b95
hassanahmed12639 <hassanonclouds@gmail.com>
2026-03-06 21:42:01 +0500

    Fix all missing pages build errors

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,15 +1,5 @@
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import { TestimonialCarousel } from "@/components/sections/TestimonialCarousel";
+import { redirect } from 'next/navigation'
 
-export default function AboutPage() {
-  return (
-    <main className="w-full min-h-screen m-0 p-0 bg-background">
-      <Header />
-      <section className="w-full m-0 py-12 md:py-16 lg:py-20 px-6 md:px-[5%]">
-        <TestimonialCarousel />
-      </section>
-      <Footer />
-    </main>
-  );
+export default function AboutMe() {
+  redirect('/')
 }
